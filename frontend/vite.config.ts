8ace308c981899b56527f6680fedb981fa0c9ea3
mayaffia <maiya.ifraimova@yandex.ru>
2025-05-08 22:14:40 +0300

    logo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,8 +7,8 @@ const manifest: false | Partial<ManifestOptions> | undefined = {
   theme_color: '#8936FF',
   background_color: '#2EC6FE',
   icons: [
-    { purpose: 'maskable', sizes: '512x512', src: 'icon512_maskable.png', type: 'image/png' },
-    { purpose: 'any', sizes: '512x512', src: 'icon512_rounded.png', type: 'image/png' },
+    { purpose: 'maskable', sizes: '512x512', src: 'logo1.png', type: 'image/png' },
+    { purpose: 'any', sizes: '512x512', src: 'logo1.png', type: 'image/png' },
   ],
   orientation: 'any',
   display: 'standalone',
