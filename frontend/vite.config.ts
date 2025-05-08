763459d7d183704c371fa4ea7ea76ff5b9fcb7b3
mayaffia <maiya.ifraimova@yandex.ru>
2025-05-08 22:05:00 +0300

    tests coverage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,26 @@ import { defineConfig } from 'vite';
 import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
 
 // manifest поменять pwa manifest generator
-const manifest: false | Partial<ManifestOptions> | undefined = {'theme_color':'#8936FF','background_color':'#2EC6FE','icons':[{'purpose':'maskable','sizes':'512x512','src':'icon512_maskable.png','type':'image/png'},{'purpose':'any','sizes':'512x512','src':'icon512_rounded.png','type':'image/png'}],'orientation':'any','display':'standalone','lang':'ru'};
+const manifest: false | Partial<ManifestOptions> | undefined = {
+  theme_color: '#8936FF',
+  background_color: '#2EC6FE',
+  icons: [
+    { purpose: 'maskable', sizes: '512x512', src: 'icon512_maskable.png', type: 'image/png' },
+    { purpose: 'any', sizes: '512x512', src: 'icon512_rounded.png', type: 'image/png' },
+  ],
+  orientation: 'any',
+  display: 'standalone',
+  lang: 'ru',
+};
 export default defineConfig({
-  plugins: [react(), VitePWA({registerType: 'autoUpdate', workbox: {
-    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-  },
-  manifest: manifest,
-})],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: manifest,
+    }),
+  ],
 });
