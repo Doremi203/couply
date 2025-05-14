45eec1577f361e22d97cf4372eef10f09a4cab87
previak <75327705+previak@users.noreply.github.com>
2025-05-14 22:03:09 +0300

    fix (#34)

--- backend/matcher/migrations/00010_add_users_geo_idx.sql
+++ backend/matcher/migrations/00010_add_users_geo_idx.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-create index concurrently if not exists idx_users_geo on users using gist (ll_to_earth(latitude, longitude));
+create index if not exists idx_users_geo on users using gist (ll_to_earth(latitude, longitude));
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-drop index concurrently if exists idx_users_geo;
+drop index if exists idx_users_geo;
 -- +goose StatementEnd
