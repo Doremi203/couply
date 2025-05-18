4663ec491ffded30a595acd11c9b952c8d5c4f2e
Doremi203 <maxim.r.ivanov@gmail.com>
2025-05-18 22:05:59 +0300

    [COUPLY-94] add length check

--- backend/auth/pkg/webapp/redirect.go
+++ backend/auth/pkg/webapp/redirect.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/Doremi203/couply/backend/auth/pkg/errors"
 	"google.golang.org/protobuf/proto"
 )
 
 func redirectHeaderMatcher(_ context.Context, w http.ResponseWriter, _ proto.Message) error {
 	headers := w.Header()
 	if location, ok := headers["Grpc-Metadata-Location"]; ok {
+		if len(location) == 0 {
+			return errors.Errorf("location header not found")
+		}
 		w.Header().Set("Location", location[0])
 		w.WriteHeader(http.StatusFound)
 	}
