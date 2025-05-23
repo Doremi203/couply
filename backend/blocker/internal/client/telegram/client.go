98b309839350ebe46a48e5997358edef2d7762ff
danilkunashev <dan_kunashev@mail.ru>
2025-05-24 02:24:03 +0300

    [COUPLY-89]: feat: better code quality for business logic

--- backend/blocker/internal/client/telegram/client.go
+++ backend/blocker/internal/client/telegram/client.go
@@ -1,30 +1,36 @@
 package telegram
 
 import (
+	"github.com/Doremi203/couply/backend/auth/pkg/errors"
+	"github.com/Doremi203/couply/backend/auth/pkg/log"
+	"github.com/Doremi203/couply/backend/blocker/internal/domain/telegram"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type BotClient struct {
 	api         *tgbotapi.BotAPI
 	adminChatID int64
-	handlers    map[string]CallbackHandler
+	handlers    map[string]telegram.CallbackHandler
+	logger      log.Logger
 }
 
-func NewBotClient(token string, chatID int64) (*BotClient, error) {
+func NewBotClient(token string, chatID int64, logger log.Logger) (*BotClient, error) {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "tgbotapi.NewBotAPI")
 	}
 
 	return &BotClient{
 		api:         bot,
 		adminChatID: chatID,
+		handlers:    make(map[string]telegram.CallbackHandler),
+		logger:      logger,
 	}, nil
 }
 
-func (b *BotClient) RegisterCallbackHandler(prefix string, handler CallbackHandler) {
+func (b *BotClient) RegisterCallbackHandler(prefix string, handler telegram.CallbackHandler) {
 	if b.handlers == nil {
-		b.handlers = make(map[string]CallbackHandler)
+		b.handlers = make(map[string]telegram.CallbackHandler)
 	}
 	b.handlers[prefix] = handler
 }
---
+++ backend/blocker/internal/domain/telegram/errors.go
@@ -0,0 +1,7 @@
+package telegram
+
+import "github.com/Doremi203/couply/backend/auth/pkg/errors"
+
+var (
+	ErrInvalidBlockIDFormat = errors.Error("invalid block id format")
+)
