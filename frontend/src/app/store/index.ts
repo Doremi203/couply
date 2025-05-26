99062bb897f24e713873d6561de9b333c82d0e69
mayaffia <maiya.ifraimova@yandex.ru>
2025-05-26 10:19:04 +0300

    [COUPLY-106] build fix

--- frontend/src/app/store/index.ts
+++ frontend/src/app/store/index.ts
@@ -1,29 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 import matchesReducer from '../../entities/matches/model/matchesSlice';
 import profileReducer from '../../entities/profile/model/profileSlice';
 import userReducer from '../../entities/user/model/userSlice';
 import filtersReducer from '../../features/filters/model/filtersSlice';
-import { baseApi, matcherApi } from '../../shared/api/baseApi';
+import { baseApi, blockerApi, matcherApi, notificatorApi, paymentsApi } from '../../shared/api/baseApi';
 
 import blockingReducer from './blockingSlice';
 
+const rootReducer = combineReducers({
+  [baseApi.reducerPath]: baseApi.reducer,
+  [matcherApi.reducerPath]: matcherApi.reducer,
+  [blockerApi.reducerPath]: blockerApi.reducer,
+  [paymentsApi.reducerPath]: paymentsApi.reducer,
+  [notificatorApi.reducerPath]: notificatorApi.reducer,
+  matches: matchesReducer,
+  filters: filtersReducer,
+  user: userReducer,
+  profile: profileReducer,
+  blocking: blockingReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    [baseApi.reducerPath]: baseApi.reducer,
-    [matcherApi.reducerPath]: matcherApi.reducer,
-    matches: matchesReducer,
-    filters: filtersReducer,
-    user: userReducer,
-    profile: profileReducer,
-    blocking: blockingReducer,
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware()
+      .concat(baseApi.middleware)
+      .concat(matcherApi.middleware)
+      .concat(blockerApi.middleware)
+      .concat(paymentsApi.middleware)
+      .concat(notificatorApi.middleware);
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(baseApi.middleware, matcherApi.middleware),
 });
 
 setupListeners(store.dispatch);
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
--- frontend/src/shared/store/index.ts
+++ frontend/src/shared/store/index.ts
@@ -1,14 +1,40 @@
+// import { configureStore } from '@reduxjs/toolkit';
+
+// import matchesReducer from '../../entities/matches/model/matchesSlice';
+// import { matcherApi } from '../api/baseApi';
+
+// export const store = configureStore({
+//   reducer: {
+//     [matcherApi.reducerPath]: matcherApi.reducer,
+//     matches: matchesReducer,
+//   },
+//   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(matcherApi.middleware),
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
+
 import { configureStore } from '@reduxjs/toolkit';
 
-import matchesReducer from '../../entities/matches/model/matchesSlice';
-import { matcherApi } from '../api/baseApi';
+import { baseApi, blockerApi, matcherApi, notificatorApi, paymentsApi } from '../api/baseApi';
+
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     [matcherApi.reducerPath]: matcherApi.reducer,
-    matches: matchesReducer,
+    [blockerApi.reducerPath]: blockerApi.reducer,
+    [paymentsApi.reducerPath]: paymentsApi.reducer,
+    [notificatorApi.reducerPath]: notificatorApi.reducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(matcherApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(baseApi.middleware)
+      .concat(matcherApi.middleware)
+      .concat(blockerApi.middleware)
+      .concat(paymentsApi.middleware)
+      .concat(notificatorApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
