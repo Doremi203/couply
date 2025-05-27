55bec29cfc34185d206457c95f29037c481e61d5
Doremi203 <maxim.r.ivanov@gmail.com>
2025-05-27 20:01:41 +0300

    hot fix: events processing

--- backend/notificator/internal/worker/matcher.go
+++ backend/notificator/internal/worker/matcher.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"context"
+	"time"
 
 	"github.com/Doremi203/couply/backend/auth/pkg/errors"
 	"github.com/Doremi203/couply/backend/auth/pkg/log"
@@ -35,6 +36,7 @@ type MatcherEventProcessor struct {
 
 func (p *MatcherEventProcessor) ProcessMessages(ctx context.Context) error {
 	for {
+		time.Sleep(time.Second * 5)
 		batch, err := p.sqsClient.ReadMessages(ctx, p.logger, 5)
 		if err != nil {
 			p.logger.Error(errors.WrapFail(err, "read messages batch from sqs"))
@@ -43,74 +45,87 @@ func (p *MatcherEventProcessor) ProcessMessages(ctx context.Context) error {
 		p.logger.Infof("read %d messages from sqs", len(batch))
 
 		for _, msg := range batch {
-			var text string
-
-			switch msg.Data.GetType() {
-			case messages.MatcherEvent_MATCH:
-				text = "У вас мэтч! Нажмите, чтобы начать общение."
-
-			case messages.MatcherEvent_LIKE:
-				text = "Вас кто-то лайкнул! Нажмите, чтобы посмотреть."
-
-			default:
-				p.logger.Error(errors.Errorf(
-					"unknown matcher %v",
-					errors.Token("event_type", msg.Data.GetType()),
-				))
-				continue
-			}
-
-			idStr, err := uuid.Parse(msg.Data.GetReceiverId())
+			err := p.processMessage(ctx, msg)
 			if err != nil {
 				p.logger.Error(errors.WrapFailf(
 					err,
-					"parse receiver id %v",
-					errors.Token("receiver_id", msg.Data.GetReceiverId()),
-				))
-				continue
-			}
-
-			pushRecipientID := push.RecipientID(idStr)
-
-			pushRecepient, err := p.subscriptionUseCase.GetRecipient(ctx, pushRecipientID)
-			if err != nil {
-				p.logger.Error(errors.WrapFailf(
-					err,
-					"get push recipient with id %v",
-					errors.Token("id", pushRecipientID),
-				))
-				continue
-			}
-			if len(pushRecepient.Subscriptions) == 0 {
-				p.logger.Infof(
-					"no subscriptions for recipient %v, skipping push",
-					errors.Token("id", pushRecipientID),
-				)
-				continue
-			}
-
-			err = p.senderUseCase.Send(ctx, pushRecepient, push.Push{
-				Title: "Couply",
-				Body:  text,
-				Icon:  "/logo.png",
-			})
-			if err != nil {
-				p.logger.Error(errors.WrapFailf(
-					err,
-					"send push to recipient with id %v",
-					errors.Token("id", pushRecipientID),
-				))
-				continue
-			}
-
-			if err := p.sqsClient.DeleteMessage(msg); err != nil {
-				p.logger.Error(errors.WrapFailf(
-					err,
-					"delete message %v",
+					"process message %v",
 					errors.Token("message_id", msg.ID),
 				))
-				continue
 			}
 		}
 	}
+}
+
+func (p *MatcherEventProcessor) processMessage(
+	ctx context.Context,
+	msg sqs.Message[*messages.MatcherEvent],
+) error {
+	defer func() {
+		if err := p.sqsClient.DeleteMessage(msg); err != nil {
+			p.logger.Error(errors.WrapFailf(
+				err,
+				"delete message %v",
+				errors.Token("message_id", msg.ID),
+			))
+		}
+	}()
+
+	var text string
+
+	switch msg.Data.GetType() {
+	case messages.MatcherEvent_MATCH:
+		text = "У вас мэтч! Нажмите, чтобы начать общение."
+
+	case messages.MatcherEvent_LIKE:
+		text = "Вас кто-то лайкнул! Нажмите, чтобы посмотреть."
+
+	default:
+		return errors.Errorf(
+			"unknown matcher %v",
+			errors.Token("event_type", msg.Data.GetType()),
+		)
+	}
+
+	idStr, err := uuid.Parse(msg.Data.GetReceiverId())
+	if err != nil {
+		return errors.WrapFailf(
+			err,
+			"parse receiver id %v",
+			errors.Token("receiver_id", msg.Data.GetReceiverId()),
+		)
+	}
+
+	pushRecipientID := push.RecipientID(idStr)
+
+	pushRecepient, err := p.subscriptionUseCase.GetRecipient(ctx, pushRecipientID)
+	if err != nil {
+		return errors.WrapFailf(
+			err,
+			"get push recipient with id %v",
+			errors.Token("id", pushRecipientID),
+		)
+	}
+	if len(pushRecepient.Subscriptions) == 0 {
+		p.logger.Infof(
+			"no subscriptions for recipient %v, skipping push",
+			errors.Token("id", pushRecipientID),
+		)
+		return nil
+	}
+
+	err = p.senderUseCase.Send(ctx, pushRecepient, push.Push{
+		Title: "Couply",
+		Body:  text,
+		Icon:  "/logo.png",
+	})
+	if err != nil {
+		return errors.WrapFailf(
+			err,
+			"send push to recipient with id %v",
+			errors.Token("id", pushRecipientID),
+		)
+	}
+
+	return nil
 }
