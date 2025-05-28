41396051561ecb16af7fbb259e8be084dcbc07b8
Ivanov Maxim Romanovich <32964154+Doremi203@users.noreply.github.com>
2025-05-28 22:58:03 +0300

    [COUPLY-122] implement valkey based rate limiting (#81)
    * [COUPLY-122] implement valkey based rate limiting
    
    * [COUPLY-122] ignore lint
    
    * [COUPLY-122] add secret

---
+++ backend/auth/pkg/webapp/ratelimit.go
@@ -0,0 +1,39 @@
+package webapp
+
+import (
+	"context"
+	"net"
+
+	"github.com/Doremi203/couply/backend/auth/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
+)
+
+type rateLimiter interface {
+	Add(ctx context.Context, key string) (bool, error)
+}
+
+func newUnaryRateLimiterInterceptor(l rateLimiter) grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req any,
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (any, error) {
+		if p, ok := peer.FromContext(ctx); ok {
+			if ip, _, err := net.SplitHostPort(p.Addr.String()); err == nil {
+				allowed, err := l.Add(ctx, ip)
+				if err != nil {
+					return nil, errors.WrapFail(err, "check rate limit")
+				}
+				if !allowed {
+					return nil, status.Errorf(codes.ResourceExhausted, "rate limit exceeded")
+				}
+			}
+		}
+
+		return handler(ctx, req)
+	}
+}
