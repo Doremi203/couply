9b78304e23f853087d41c4c53dda03629915ac97
danilkunashev <dan_kunashev@mail.ru>
2025-05-29 03:07:18 +0300

    [COUPLY-126]: feat: change tg get

--- backend/auth/internal/usecase/telegram/get_telegram_data.go
+++ backend/auth/internal/usecase/telegram/get_telegram_data.go
@@ -3,22 +3,33 @@ package telegram
 import (
 	"context"
 
+	"github.com/google/uuid"
+
 	"github.com/Doremi203/couply/backend/auth/pkg/errors"
 	"github.com/Doremi203/couply/backend/auth/pkg/token"
 )
 
-func (u UseCase) GetTelegramData(ctx context.Context) (string, error) {
-	userID, err := token.GetUserIDFromContext(ctx)
+const (
+	tgURLFirstPart = "https://t.me/"
+)
+
+func (u UseCase) GetTelegramData(ctx context.Context, userID string) (string, error) {
+	_, err := token.GetUserIDFromContext(ctx)
 	if err != nil {
 		return "", errors.Wrap(err, "token.GetUserIDFromContext")
 	}
 
-	tgData, err := u.telegramRepo.GetTelegramData(ctx, userID)
+	userUUID, err := uuid.Parse(userID)
+	if err != nil {
+		return "", errors.Wrap(err, "uuid.Parse")
+	}
+
+	tgData, err := u.telegramRepo.GetTelegramData(ctx, userUUID)
 	if err != nil {
 		return "", errors.Wrap(err, "u.telegramRepo.SetTelegramData")
 	}
 
-	tgURL := "https://t.me/" + tgData.TelegramUsername
+	tgURL := tgURLFirstPart + tgData.TelegramUsername
 
 	return tgURL, nil
 }
