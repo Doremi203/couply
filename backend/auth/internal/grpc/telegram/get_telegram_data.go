e11c9be9204d951e2bf2480c1eb462996717a629
previak <75327705+previak@users.noreply.github.com>
2025-05-29 03:12:30 +0300

    [COUPLY-126] (#88)
    [COUPLY-126]: feat: change tg get

--- backend/auth/internal/grpc/telegram/get_telegram_data.go
+++ backend/auth/internal/grpc/telegram/get_telegram_data.go
@@ -3,6 +3,8 @@ package telegram
 import (
 	"context"
 
+	telegram2 "github.com/Doremi203/couply/backend/auth/internal/repo/telegram"
+
 	"github.com/Doremi203/couply/backend/auth/gen/api/telegram"
 	"github.com/Doremi203/couply/backend/auth/pkg/errors"
 	"github.com/Doremi203/couply/backend/auth/pkg/token"
@@ -12,15 +14,17 @@ import (
 
 func (s *grpcService) GetTelegramDataV1(
 	ctx context.Context,
-	_ *telegram.GetTelegramDataV1Request,
+	req *telegram.GetTelegramDataV1Request,
 ) (*telegram.GetTelegramDataV1Response, error) {
 	_, ok := token.FromContext(ctx)
 	if !ok {
 		return nil, status.Error(codes.Unauthenticated, "missing token")
 	}
 
-	url, err := s.useCase.GetTelegramData(ctx)
+	url, err := s.useCase.GetTelegramData(ctx, req.GetUserId())
 	switch {
+	case errors.Is(err, telegram2.ErrTelegramDataNotFound):
+		return nil, status.Error(codes.NotFound, telegram2.ErrTelegramDataNotFound.Error())
 	case err != nil:
 		s.logger.Error(errors.Wrap(err, "get telegram data v1 failed"))
 		return nil, status.Error(codes.Internal, "internal error")
